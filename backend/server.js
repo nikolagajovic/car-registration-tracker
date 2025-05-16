8a3142923eff5358a28ba911969eb5d153abe219
nikolagajovic <iamnikolagajovic@gmail.com>
2025-05-16 21:59:52 +0200

    fix database

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
-
 
 //Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'car_registration_tracker'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+         rejectUnauthorized: false 
+    }
 });
 
 db.connect((err) => {
